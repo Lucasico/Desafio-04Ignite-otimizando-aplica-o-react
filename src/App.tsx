4e9513837f80f066ddf4d64747db6f6f5de15f8c
lucas <lucassantoscrfbezerra@gmail.com>
2021-09-11 19:22:37 -0300

    chore: melhorando performance dos components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState, useCallback } from 'react';
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
-
 import { api } from './services/api';
-
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
@@ -51,9 +47,10 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  },[])
+  
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
